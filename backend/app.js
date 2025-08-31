234474ec06855373027db7faad9c5d468ee0a1aa
Santiagio Ayala <santitomayala07@gmail.com>
2025-08-31 18:57:39 -0300

    Agregación del express.json() para que lea el req.body

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,16 @@
 import express from "express";
 import { startDB } from "./src/config/db.js";
+import personRouter from "./src/routes/person.routes.js";
+import userRouter from "./src/routes/user.routes.js";
 
 startDB();
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.use("/", (req, res) => {
-  //Rutas
-});
+app.use(express.json());
+app.use("/", personRouter, userRouter);
 
 app.listen(PORT, () => {
-  console.log(`Escuchando en el puerto: ${PORT}`);
+  console.log(`Escuchando en el puerto: http://localhost:${PORT}`);
 });
