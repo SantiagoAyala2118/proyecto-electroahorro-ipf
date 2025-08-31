ceb408d05133845f6158ed039cd57a1e26dcdc5b
Santiagio Ayala <santitomayala07@gmail.com>
2025-08-31 16:48:24 -0300

    Se agregó la instancia where en la función custom y se agreg+o la validación de existencia de person

--- backend/src/middlewares/validations/userValidations/createUser.validation.js
+++ backend/src/middlewares/validations/userValidations/createUser.validation.js
@@ -1,5 +1,6 @@
 import { body } from "express-validator";
 import { UserModel } from "../../../models/user.model.js";
+import { PersonModel } from "../../../models/person.model.js";
 
 export const createUserValidations = [
   body("username")
@@ -12,9 +13,11 @@ export const createUserValidations = [
     .withMessage(
       "Username must contain at least 3 characters and a maximum of 10"
     )
-    .custom(async (username) => {
+    .custom(async (username, { req }) => {
       try {
-        const existingUsername = await UserModel.findOne({ where: username });
+        const existingUsername = await UserModel.findOne({
+          where: { username: req.body.username },
+        });
         if (existingUsername) {
           return Promise.reject("There is already an user with that username");
         }
@@ -34,9 +37,11 @@ export const createUserValidations = [
     .withMessage("Email cannot be empty")
     .isEmail()
     .withMessage("The email must have a valid format")
-    .custom(async (email) => {
+    .custom(async (email, { req }) => {
       try {
-        const existingEmail = await UserModel.findOne({ where: email });
+        const existingEmail = await UserModel.findOne({
+          where: { email: req.body.email },
+        });
         if (existingEmail) {
           return Promise.reject("There is already an user with that email");
         }
@@ -57,4 +62,28 @@ export const createUserValidations = [
     .withMessage(
       "The password must contain at least 8 characters and a maximum of 16"
     ),
+  param("person_id")
+    .trim()
+    .notEmpty()
+    .withMessage("The person_id cannot be empty")
+    .isInt({ gt: 0 })
+    .withMessage("The person_id must be a number greater than 0")
+    .custom(async (person_id, { req }) => {
+      try {
+        const existingPerson = await PersonModel.findOne({
+          where: { person_id: req.params.person_id },
+        });
+        if (!existingPerson) {
+          return Promise.reject("There is no person with that id in the DB");
+        }
+      } catch (err) {
+        console.error(
+          "Error while checking the existency of that person in the DB",
+          err
+        );
+        return Promise.reject(
+          "Error while checking the existency of that person in the DB"
+        );
+      }
+    }),
 ];
