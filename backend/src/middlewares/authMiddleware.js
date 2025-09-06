00f002dc7a4ab1030efeb8b90ae9dd9abd9fecb0
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-06 18:19:49 -0300

    Middleware de autenticación terminado

---
+++ backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,24 @@
+import { verifyToken } from "../helpers/jwt.helper.js";
+
+export const authMiddleware = async (req, res, next) => {
+  try {
+    const token = req.cookie["token"];
+
+    if (!token) {
+      return res.stats(401).json({
+        message: "User not authenticated",
+      });
+    }
+
+    const decoded = verifyToken(token);
+
+    req.userLogged = decoded;
+
+    next();
+  } catch (err) {
+    console.error("Error checking the authentication", err);
+    return res.status(500).json({
+      message: "Error checking the authentication",
+    });
+  }
+};
