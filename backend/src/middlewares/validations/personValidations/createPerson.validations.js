83a622c53b51ba81c1deba03199896ce0ef4abd8
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-06 18:17:02 -0300

    Validaciones actualizadas

--- backend/src/middlewares/validations/personValidations/createPerson.validations.js
+++ backend/src/middlewares/validations/personValidations/createPerson.validations.js
@@ -1,44 +1,76 @@
 import { body } from "express-validator";
-import { PersonModel } from "../../../models/person.model.js";
+import { UserModel } from "../../../models/user.model.js";
 
 export const createPersonValidations = [
-  body("name")
+  body("fullname")
     .trim()
     .notEmpty()
-    .withMessage("Name cannot be empty")
+    .withMessage("The fullname cannot be empty")
     .isString()
-    .withMessage("Name must be a string")
-    .isLength({ min: 5, max: 10 })
-    .withMessage("Name must contain at least 5 characters and a maximun of 10"),
-  body("lastname")
-    .trim()
-    .notEmpty()
-    .withMessage("Lastname cannot be empty")
-    .isString()
-    .withMessage("Lastname must be a string")
-    .isLength({ min: 5, max: 10 })
-    .withMessage("Name must contain at least 5 characters and a maximun of 10"),
-  body("dni")
-    .trim()
-    .notEmpty()
-    .withMessage("Lastname cannot be empty")
-    .isInt({ gt: 30000000 })
+    .withMessage("The fullname must be a string")
+    .isLength({ min: 5, max: 50 })
     .withMessage(
-      "DNI must be a number greater than 30,000,000 (thirty-millions) with no points"
+      "The fullname must contain at least 5 characters and a maximun of 50"
     )
-    .isLength({ min: 5, max: 10 })
-    .withMessage("DNI must contain 8 digits")
-    .custom(async (dni, { req }) => {
+    .matches(/^([A-ZÁÉÍÓÚÑ][a-záéíóúñ]+)(\s[A-ZÁÉÍÓÚÑ][a-záéíóúñ]+)*$/)
+    .withMessage(
+      "The fullname must contain only letters, start with capital and be sparated by spaces"
+    ),
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("The email cannot be empty")
+    .isEmail()
+    .withMessage("Email format invalid")
+    .custom(async (email, { req }) => {
       try {
-        const dniExisting = await PersonModel.findOne({
-          where: { dni: req.body.dni },
+        const emailExisting = await UserModel.findOne({
+          where: { email: req.body.email },
         });
-        if (dniExisting) {
-          return Promise.reject("DNI already exists in the DB");
+
+        if (emailExisting) {
+          return Promise.reject(
+            "There is already an email like that, try another one"
+          );
         }
       } catch (err) {
-        console.error("Server error while checking the DNI viability", err);
-        return Promise.reject("Server error while checking the DNI viability");
+        console.error("Error checking the viability of the email", err);
+        return Promise.reject("Error checking the viability of the email");
+      }
+    }),
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("The password cannot be empty")
+    .isString()
+    .withMessage("The password must be a string")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&._-])[A-Za-z\d@$!%*?&._-]{8,}$/
+    )
+    .withMessage(
+      "The password must contain at leas 8 characters, one number, one sepecial character, capital and lower case"
+    ),
+  body("confirm_password")
+    .trim()
+    .notEmpty()
+    .withMessage("The confirm_password field cannot be empty")
+    .isString()
+    .withMessage("The confirm_password field must be a string")
+    .custom(async (confirm_password, { req }) => {
+      try {
+        if (req.body.confirm_password !== req.body.password) {
+          return Promise.reject(
+            "The password and the confirmation password are different"
+          );
+        }
+      } catch (err) {
+        console.error(
+          "Error checking the coincidens between the password and the confirmation of the password",
+          err
+        );
+        return Promise.reject(
+          "Error checking the coincidens between the password and the confirmation of the password"
+        );
       }
     }),
 ];
