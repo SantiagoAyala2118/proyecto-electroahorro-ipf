bc1c6964357249dec1e390874d4e9e96eb0beca4
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-07 14:23:28 -0300

    Se mandaron los endpoints de perfil a otro archivo

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,17 +1,10 @@
 import { Router } from "express";
 import { login, logout, register } from "../controllers/auth.controller.js";
-import {
-  getOneProfile,
-  updateProfile,
-} from "../controllers/profile.controller.js";
-import { updateUser, deletUser } from "../controllers/user.controller.js";
+
 //-------------------------------------------------------------MIDDLEWARES
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 import { applyValidations } from "../middlewares/validator.js";
 import { createPersonValidations } from "../middlewares/validations/personValidations/createPerson.validations.js";
-import { updateProfileValidations } from "../middlewares/validations/profileValidations/updateProfile.validations.js";
-import { updateUserValidations } from "../middlewares/validations/userValidations/updateUser.validations.js";
-import { deleteUserValidations } from "../middlewares/validations/userValidations/deleteUser.validations.js";
 
 const authRouter = Router();
 
@@ -25,36 +18,6 @@ authRouter.post(
 
 //Loguearse
 authRouter.post("/api/auth/login", login);
-
-//Ver el perfil propio
-authRouter.get("/api/auth/profile", authMiddleware, getOneProfile);
-
-//Actualizar el perfil
-authRouter.put(
-  "/api/auth/profile",
-  authMiddleware,
-  updateProfileValidations,
-  applyValidations,
-  updateProfile
-);
-
-//Actualizar el usuario
-authRouter.put(
-  "/api/user/profile",
-  authMiddleware,
-  updateUserValidations,
-  applyValidations,
-  updateUser
-);
-
-//Eliminar el usuario
-authRouter.delete(
-  "/api/user/profile",
-  authMiddleware,
-  deleteUserValidations,
-  applyValidations,
-  deletUser
-);
 
 //Desloguearse (cerrar sesión)
 authRouter.post("/api/auth/logout", authMiddleware, logout);
