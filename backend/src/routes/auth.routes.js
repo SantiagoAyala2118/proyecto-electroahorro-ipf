abfffc84006cc41677c477a1d37046e274f895b4
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-07 14:00:23 -0300

    Se migraron las rutas importantes de usuario y profile para tener un mejor control de los endpoints

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -4,13 +4,18 @@ import {
   getOneProfile,
   updateProfile,
 } from "../controllers/profile.controller.js";
+import { updateUser, deletUser } from "../controllers/user.controller.js";
 //-------------------------------------------------------------MIDDLEWARES
 import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { applyValidations } from "../middlewares/validator.js";
 import { createPersonValidations } from "../middlewares/validations/personValidations/createPerson.validations.js";
-import applyValidations from "../middlewares/validator.js";
 import { updateProfileValidations } from "../middlewares/validations/profileValidations/updateProfile.validations.js";
+import { updateUserValidations } from "../middlewares/validations/userValidations/updateUser.validations.js";
+import { deleteUserValidations } from "../middlewares/validations/userValidations/deleteUser.validations.js";
+
 const authRouter = Router();
 
+//Rregistrarse
 authRouter.post(
   "/api/auth/registro",
   createPersonValidations,
@@ -18,10 +23,13 @@ authRouter.post(
   register
 );
 
+//Loguearse
 authRouter.post("/api/auth/login", login);
 
+//Ver el perfil propio
 authRouter.get("/api/auth/profile", authMiddleware, getOneProfile);
 
+//Actualizar el perfil
 authRouter.put(
   "/api/auth/profile",
   authMiddleware,
@@ -30,6 +38,25 @@ authRouter.put(
   updateProfile
 );
 
+//Actualizar el usuario
+authRouter.put(
+  "/api/user/profile",
+  authMiddleware,
+  updateUserValidations,
+  applyValidations,
+  updateUser
+);
+
+//Eliminar el usuario
+authRouter.delete(
+  "/api/user/profile",
+  authMiddleware,
+  deleteUserValidations,
+  applyValidations,
+  deletUser
+);
+
+//Desloguearse (cerrar sesión)
 authRouter.post("/api/auth/logout", authMiddleware, logout);
 
 export default authRouter;
