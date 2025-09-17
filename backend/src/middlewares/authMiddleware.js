1eeb1cf37a8165c850a6dd5bff5e655e81dda8f9
Santiago Ayala <santitomayala07@gmail.com>
2025-09-17 19:24:51 -0300

    Corrección en la escritura  del status del error

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -5,7 +5,7 @@ export const authMiddleware = async (req, res, next) => {
     const token = req.cookies["token"];
 
     if (!token) {
-      return res.stats(401).json({
+      return res.status(401).json({
         message: "User not authenticated",
       });
     }
