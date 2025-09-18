d763eab83cef9c33b0957b09be62fee59f737454
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-18 15:49:24 -0300

    Conexion a la BD corregida

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,7 +4,7 @@ export const startDB = async () => {
   try {
     await sequelize.authenticate();
     console.log("Conexión exitosa a la Base de datos");
-    await sequelize.sync({force:true});
+    await sequelize.sync({ force: true });
     console.log("Sincronización existosa con la Base de datos");
   } catch (err) {
     console.error("No se ha podido conectar con la Base de datos", err);
