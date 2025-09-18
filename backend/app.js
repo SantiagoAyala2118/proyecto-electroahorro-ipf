8800b909c8bee5d2db3ec9ed4e7b83c32fadc3a8
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-18 14:39:10 -0300

    Se importó la ruta de authenticación

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { startDB } from "./src/config/db.js";
-import personRouter from "./src/routes/person.routes.js";
-import userRouter from "./src/routes/user.routes.js";
+import authRouter from "./src/routes/auth.routes.js";
 
 startDB();
 
@@ -9,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-app.use("/", personRouter, userRouter);
+app.use("/", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Escuchando en el puerto: http://localhost:${PORT}`);
