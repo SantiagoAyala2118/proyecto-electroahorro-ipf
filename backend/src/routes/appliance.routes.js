a693c430a762c80e4a25e16d835ea6439f94e023
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-25 16:44:23 -0300

    Rutas de appliances agregadas

--- backend/src/routes/appliance.routes.js
+++ backend/src/routes/appliance.routes.js
@@ -1,21 +1,36 @@
-import Router from 'express'
+import Router from "express";
 
 //--------------------------CONTROLADORES
-import { createAppliance } from '../controllers/appliance.controller.js';
+import {
+  createAppliance,
+  deleteAppliance,
+  getAllApliances,
+  getAppliance,
+  updateAppliance,
+} from "../controllers/appliance.controller.js";
 
 //--------------------------MIDDLEWARES
-import { createApplianceValidations } from '../middlewares/validations/applianceValidations/createAppliance.validations.js';
-import { applyValidations } from '../middlewares/validator.js';
-import { authMiddleware } from '../middlewares/authMiddleware.js';
+import { createApplianceValidations } from "../middlewares/validations/applianceValidations/createAppliance.validations.js";
+import { applyValidations } from "../middlewares/validator.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const applianceRouter = Router();
 
 //CREAR ELECTRODOMESTICO
-applianceRouter.post('/api/appliance',
-    authMiddleware,
-    createApplianceValidations,
-    applyValidations,
-    createAppliance
-)
+applianceRouter.post(
+  "/appliance",
+  authMiddleware,
+  createApplianceValidations,
+  applyValidations,
+  createAppliance
+);
+
+applianceRouter.get("/appliance", authMiddleware, getAllApliances);
+
+applianceRouter.get("/appliance/:id", authMiddleware, getAppliance);
+
+applianceRouter.put("/appliance/:id", authMiddleware, updateAppliance);
+
+applianceRouter.delete("/appliance/:id", authMiddleware, deleteAppliance);
 
 export default applianceRouter;
