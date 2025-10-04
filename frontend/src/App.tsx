abe1632e7b89089c232d37ffc26021087e73ee2b
Santiagio Ayala <santitomayala07@gmail.com>
2025-10-04 13:21:34 -0300

    Ya no se renderiza el componente Registro en el archivo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,11 @@
 import "./App.css";
 import Home from "./components/pages/Home.tsx";
 import Hero from "./components/Hero/Hero.tsx";
-import Registro from "./Registro.tsx";
-
-const App = () => {
+export const App = () => {
   return (
     <div>
       <Home />
       <Hero />
-      <Registro />
     </div>
   );
 };
-
-export default App;
