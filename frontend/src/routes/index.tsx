0ca1637190292b7ec14933c09962a140c2852fbf
Santiagio Ayala <santitomayala07@gmail.com>
2025-10-04 11:32:20 -0300

    Archivo index de rutas para navegar por las paginas creado

---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,12 @@
+// * Se importan los componentes a utilizar
+import { createBrowserRouter } from "react-router-dom";
+import Registro from "../Registro.tsx";
+import Login from "../Login.tsx";
+import Home from "../components/pages/Home.tsx";
+
+// * Se hace una onstante router en donde se guardan todas las direcciones para ir navegando por los componentes
+export const router = createBrowserRouter([
+  { path: "/", element: <Registro /> },
+  { path: "/login", element: <Login /> },
+  { path: "/app", element: <Home /> },
+]);
