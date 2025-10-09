95f1185072a174ddac1bdd0630b4002b8199b776
Santiagio Ayala <santitomayala07@gmail.com>
2025-10-09 16:19:45 -0300

    Corrección en las importacinones de las rutas

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,7 +1,7 @@
 // * Se importan los componentes a utilizar
 import { createBrowserRouter } from "react-router-dom";
-import Registro from "../Registro.tsx";
-import Login from "../Login.tsx";
+import Registro from "../components/Registro/Registro.tsx";
+import Login from "../components/Login/Login.tsx";
 import Home from "../components/pages/Home.tsx";
 
 // * Se hace una onstante router en donde se guardan todas las direcciones para ir navegando por los componentes
